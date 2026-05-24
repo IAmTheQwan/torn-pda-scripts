b87253058024567f87e9aeeb5e0fd83d426860bb
IAmTheQwan <theqwan630@gmail.com>
2026-05-24 12:36:45 -0400

    Create theqwan-caf-helper.meta.js

---
+++ theqwan-caf-helper.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         TheQwan CAF History Helper
+// @namespace    theqwan.torn.auction-history.caf35
+// @version      3.5.6
+// @description  Historical comp pricing helper for TheQwan CAF Base
+// @author       TheQwan
+// @match        https://www.torn.com/amarket.php*
+// @match        https://www.torn.com/page.php*
+// @grant        GM_xmlhttpRequest
+// @connect      btrmmuuoofbonmuwrkzg.supabase.co
+// @updateURL    https://raw.githubusercontent.com/IAmTheQwan/torn-caf-scripts/main/theqwan-caf-helper.meta.js
+// @downloadURL  https://raw.githubusercontent.com/IAmTheQwan/torn-caf-scripts/main/theqwan-caf-helper.user.js
+// ==/UserScript==
