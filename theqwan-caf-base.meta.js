f98e505a207f078aa27a7c3663910e6401b62c01
IAmTheQwan <theqwan630@gmail.com>
2026-05-24 12:34:25 -0400

    Create theqwan-caf-base.meta.js

---
+++ theqwan-caf-base.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         TheQwan CAF Base
+// @namespace    theqwan.torn.auction-filter.caf3
+// @version      3.4.6
+// @description  Condensed Auction Filter with quality fetch, color filter, and slider-based quality/bonus ranges
+// @author       TheQwan [3485263]
+// @match        https://www.torn.com/amarket.php*
+// @match        https://www.torn.com/page.php*
+// @updateURL    https://raw.githubusercontent.com/IAmTheQwan/torn-caf-scripts/main/theqwan-caf-base.user.js
+// @downloadURL  https://raw.githubusercontent.com/IAmTheQwan/torn-caf-scripts/main/theqwan-caf-base.user.js
+// ==/UserScript==
