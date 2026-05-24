39a8be96dc01727ba3fb2788b839239c8385deca
IAmTheQwan <theqwan630@gmail.com>
2026-05-24 12:25:44 -0400

    Update theqwan-caf-base.user.js
    Author Change

--- theqwan-caf-base.user.js
+++ theqwan-caf-base.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         TheQwan CAF Base
 // @namespace    theqwan.torn.auction-filter.caf3
-// @version      3.4.5
+// @version      3.4.6
 // @description  Condensed Auction Filter with quality fetch, color filter, and slider-based quality/bonus ranges
-// @author       TheQwan
+// @author       TheQwan [3485263]
 // @match        https://www.torn.com/amarket.php*
 // @match        https://www.torn.com/page.php*
 // @updateURL    https://raw.githubusercontent.com/IAmTheQwan/torn-caf-scripts/main/theqwan-caf-base.user.js
