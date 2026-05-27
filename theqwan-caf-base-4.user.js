72a4175704edab861f1e04f762d090e31ac48735
IAmTheQwan <theqwan630@gmail.com>
2026-05-27 15:44:55 -0400

    Update theqwan-caf-base-4.user.js

--- theqwan-caf-base-4.user.js
+++ theqwan-caf-base-4.user.js
@@ -3,7 +3,7 @@
 // @namespace    theqwan.torn.auction-filter.caf4
 // @version      4.0.8.3
 // @description  Global CAF watch banner with auction filter/history/watch system
-// @author       TheQwanTT [3485263]
+// @author       TheQwan [3485263]
 // @match        https://www.torn.com/*
 // @match        https://www.torn.com/amarket.php*
 // @match        https://www.torn.com/page.php*
