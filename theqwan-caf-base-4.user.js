e0ddbc27a8bd0b1bc0053bfef50f7fbedd2ab536
IAmTheQwan <theqwan630@gmail.com>
2026-05-27 20:53:07 -0400

    Update theqwan-caf-base-4.user.js

--- theqwan-caf-base-4.user.js
+++ theqwan-caf-base-4.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         TheQwan CAF Base 4.0 Beta
 // @namespace    theqwan.torn.auction-filter.caf4
-// @version      4.1.1.1
+// @version      4.1.1.2
 // @description  Global CAF watch banner with auction filter/history/watch system
 // @author       TheQwan [3485263]
 // @match        https://www.torn.com/*
 // @match        https://www.torn.com/amarket.php*
 // @match        https://www.torn.com/page.php*
 // @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
 // @connect      btrmmuuoofbonmuwrkzg.supabase.co
 // @updateURL    https://raw.githubusercontent.com/IAmTheQwan/torn-pda-scripts/main/theqwan-caf-base-4.meta.js
 // @downloadURL  https://raw.githubusercontent.com/IAmTheQwan/torn-pda-scripts/main/theqwan-caf-base-4.user.js
@@ -2510,7 +2511,7 @@ async function cafHistoryRun(item, scope = document) {
   return candidates;
 }
 
-  window.cafDebugBottomLayers = cafDebugBottomLayers;
+  unsafeWindow.cafDebugBottomLayers = cafDebugBottomLayers;
 
 renderGlobalWatchBar();
 completePendingAuctionJump();
