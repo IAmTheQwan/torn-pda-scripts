56597cdedf4d379e4c459c9f72553bc8f1c299c3
TheQwan <theqwan630@gmail.com>
2026-08-08 02:27:17 -0400

    Bypass stale Bookie download cache

--- qwantum-bookie.user.js
+++ qwantum-bookie.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Torn PDA Bookie Panel
-// @version      1.2.7
+// @version      1.2.8
 // @description  Floating PDA panel for Torn bookie open bets, daily totals, net, and batch tracking
 // @author       TheQwan
 // @match        https://www.torn.com/*
 // @grant        GM_xmlhttpRequest
 // @connect      api.torn.com
 // @updateURL    https://raw.githubusercontent.com/IAmTheQwan/torn-pda-scripts/Bookie/qwantum-bookie.meta.js
-// @downloadURL  https://raw.githubusercontent.com/IAmTheQwan/torn-pda-scripts/Bookie/qwantum-bookie.user.js
+// @downloadURL  https://github.com/IAmTheQwan/torn-pda-scripts/raw/refs/heads/Bookie/qwantum-bookie.user.js
 // ==/UserScript==
 
 (function () {
